4b9ce75ea44c841e832086a56ba22954aa9a7716
[ghayastanoli] <[ghiyastanoli@yahoo.com]>
2025-01-28 02:06:54 +0500

    chore: first commit

---
+++ src/components/Cards.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import king from '../assets/cards/K.png'
+import queen from '../assets/cards/Q.png'
+import jack from '../assets/cards/J.png'
+import king from '../assets/cards/k.png'
+import king from '../assets/cards/k.png'
+
+const Cards = () => {
+  return (
+    <>
+    
+    </>
+  )
+}
+
+export default Cards
